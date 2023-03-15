9b4bf8c7ee955560b23b45d17757f73be519ac5f
Felipe <Pinheirofelipe741@gmail.com>
2023-03-15 09:39:10 -0300

    HI

--- java.js
+++ java.js
@@ -1,13 +1,16 @@
 let valor_cidade = [] // variavel global do tipo array
+let res = document.getElementById('res')
 
 function inserir(){
     let cidade = document.getElementById('cidade').value
    valor_cidade.push(cidade)
     console.log(valor_cidade)
+    res.InnerHTML = valor_cidade
 }
 
 function excluir(){
     let cidade = document.getElementById('cidade').value
    valor_cidade.pop(cidade)
     console.log(valor_cidade)
+    res.InnerHTML = valor_cidade
 }
